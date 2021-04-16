a45b4d90fd58d4bcd3ed74e167605b94508b877c
L Caraco <lbc.dna@gmail.com>
2021-04-16 18:41:43 -0500

    commentary

--- src/Data.elm
+++ src/Data.elm
@@ -20,16 +20,16 @@ type alias VeggieDict a =
     }
 
 type Objective
-    = Combo (List Veggie) Points -- TODO: split into Distinct combo, nx Combo
-    | Stacked Veggie Int Points
-    | Items (VeggieDict Points)
-    | Most Veggie Points
-    | Fewest Veggie Points
-    | FewestTotal Points
-    | MostTotal Points
-    | PerTypeWith Int Points
-    | PerMissing Points
-    | EvenOdd Veggie Points Points  -- always 7 3...
+    = Combo (List Veggie) Points   -- always len 2, 3, or 6
+    | Stacked Veggie Int Points  -- always _ 2 5 or _ 3 8
+    | Items (VeggieDict Points) -- always len 1, 2, or 3
+    | Most Veggie Points  -- always _ 10
+    | Fewest Veggie Points  -- always _ 7
+    | MostTotal Points  -- always 10
+    | FewestTotal Points  -- always 7
+    | PerTypeWith Int Points  -- always 2 3 or 3 5
+    | PerMissing Points  -- always 5
+    | EvenOdd Veggie Points Points  -- always _ 7 3
 
 
 type Card = Card Int Veggie Objective
