ee61f6e89b7e3e4b49c93eeb28c179ea7eae511f
L Caraco <lbc.dna@gmail.com>
2021-04-18 11:07:39 -0500

    expanded Msg

--- src/Message.elm
+++ src/Message.elm
@@ -1,5 +1,18 @@
 module Message exposing (..)
-import Vector3
+import Vector3 exposing (Index)
+import Card exposing (Card)
+import Veggie exposing (Veggie)
+
+type alias Selection =
+    { aisle : Index
+    , item : Result Card { veggie : Veggie, first : Bool }
+    }
 
 type Msg 
-    = Selected Vector3.Index (Maybe Bool)
+    = Selected Selection
+
+selectObjective : Index -> Card -> Msg
+selectObjective i c = Selected { item = Err c, aisle = i }
+
+selectVeggie : Index -> Veggie -> Bool -> Msg
+selectVeggie i v first = Selected { item = Ok { veggie = v, first = first }, aisle = i }
