afe58320d4eb0055eeaffddfbbc3e866cec3ae8a
L Caraco <lbc.dna@gmail.com>
2021-04-18 20:51:11 -0500

    helpful list stuff

--- src/Utils.elm
+++ src/Utils.elm
@@ -17,3 +17,23 @@ const x _ = x
 
 maybe : b -> (a -> b) -> Maybe a -> b
 maybe x f = Maybe.withDefault x << Maybe.map f
+
+split : (a -> b) -> (a -> c) -> a -> (b, c)
+split f g x = (f x, g x)
+
+groupBy : (a -> a -> Bool) -> List a -> List (List a)
+groupBy f items =
+    case items of
+        [] -> []
+        h :: t -> 
+            let
+                (eqis, neqis) = List.partition (f h) t
+                front = if List.length eqis > 0 then (::) eqis else identity
+            in
+                front <| groupBy f neqis
+
+group : List a -> List (List a)
+group = groupBy (==)
+
+count : List a -> List (a, Int)
+count = List.filterMap (\ls -> Maybe.map (\x -> (x, List.length ls)) (List.head ls)) << group
