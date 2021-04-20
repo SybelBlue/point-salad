408a6698af6b97207fbea90cbd3956d5b30c5637
L Caraco <lbc.dna@gmail.com>
2021-04-19 20:07:52 -0500

    changed IdentList

--- ImportCleaner.hs
+++ ImportCleaner.hs
@@ -6,7 +6,8 @@ import Text.Parsec.String
 import Text.Parsec
 import Control.Applicative (liftA2)
 import Data.Bifunctor 
-import Data.List (intercalate)
+import Data.List (nub, intercalate)
+import qualified Data.Map.Strict as M
 
 whitespace = " \t\n"
 
@@ -17,12 +18,23 @@ identifier = lexeme $ many1 (alphaNum <|> oneOf "._")
 
 exposing = lexeme $ string "exposing"
 
-type IdentList = Maybe [IListItem]
+data IdentList 
+    = All
+    | List [IListItem]
+    deriving (Show, Eq)
+
+intoMaybe :: IdentList -> Maybe [IListItem]
+intoMaybe All = Nothing
+intoMaybe (List ls) = Just ls
 
 data IListItem
     = Simple String
     | Recurs String IdentList
-    deriving Show
+    deriving (Show, Eq)
+
+intoTuple :: IListItem -> (String, Maybe IdentList)
+intoTuple (Simple s) = (s, Nothing)
+intoTuple (Recurs s list) = (s, Just list)
 
 importList :: Parser IdentList
 importList =
@@ -33,7 +45,7 @@ importList =
   where
     importBody :: Parser IdentList
     importBody = 
-        lexeme (Nothing <$ string "..") <|> fmap Just (importIdentifier `sepBy` lexeme (char ','))
+        lexeme (All <$ string "..") <|> fmap List (importIdentifier `sepBy` lexeme (char ','))
     importIdentifier :: Parser IListItem
     importIdentifier =
      do base <- identifier
@@ -48,15 +60,29 @@ headerLine s =
 
 header = (,) <$> headerLine "module" <*> many (headerLine "import")
 
+cleanIdentList :: IdentList -> IdentList
+cleanIdentList = 
+    \case
+        All -> All
+        List list -> undefined 
+            where
+                m = M.fromListWith insertFn $ map intoTuple list
+                insertFn :: Maybe IdentList -> Maybe IdentList -> Maybe IdentList
+                insertFn _ (Just All) = Just All
+                insertFn (Just All) _ = Just All
+                insertFn Nothing o = o
+                insertFn o Nothing = o
+                insertFn (Just (List as)) (Just (List bs)) = Just . List . reverse . nub . reverse $ (as ++ bs)
 
+formatModule :: (String, IdentList) -> String
 formatModule (name, exports) = "module " ++ name ++ " exposing " ++ formatIdentList exports
 
+formatImport :: (String, IdentList) -> String
 formatImport (name, imports) = "import " ++ name ++ " exposing " ++ formatIdentList imports
 
-formatIdentList list = "( " ++ maybe ".." body list ++ " )"
-    where body = intercalate " , " . map (\case
-            Simple s -> s
-            Recurs s list -> s ++ ' ':formatIdentList list)
+formatIdentList :: IdentList -> String
+formatIdentList list = "( " ++ maybe ".." body (intoMaybe list) ++ " )"
+    where body = intercalate " , " . map ((\(s, ma) -> s ++ maybe "" ((' ':) . formatIdentList) ma) . intoTuple)
 
 formatAll m is = intercalate "\n" $ formatModule m : "\n" : map formatImport is
 
