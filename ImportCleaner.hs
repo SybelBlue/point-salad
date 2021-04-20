c09e2ca907b9c30aaf33be6bbff4d6ee3b89f5b3
L Caraco <lbc.dna@gmail.com>
2021-04-20 10:36:09 -0500

    progress on importcleaner

--- ImportCleaner.hs
+++ ImportCleaner.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE FlexibleContexts #-}
 module ImportCleaner where
 
@@ -6,8 +5,9 @@ import Text.Parsec.String
 import Text.Parsec
 import Control.Applicative (liftA2)
 import Data.Bifunctor 
-import Data.List (nub, intercalate)
+import Data.List (nub, intercalate, sort, sortOn)
 import qualified Data.Map.Strict as M
+import Data.Char (isUpper)
 
 whitespace = " \t\n"
 
@@ -23,18 +23,43 @@ data IdentList
     | List [IListItem]
     deriving (Show, Eq)
 
+instance Ord IdentList where
+    All <= (List _) = False
+    _ <= _ = True
+
 intoMaybe :: IdentList -> Maybe [IListItem]
 intoMaybe All = Nothing
 intoMaybe (List ls) = Just ls
+
+fromMaybe :: Maybe [IListItem] -> IdentList
+fromMaybe = maybe All List
 
 data IListItem
     = Simple String
     | Recurs String IdentList
     deriving (Show, Eq)
 
+instance Ord IListItem where
+    (Recurs a xs) <= (Recurs b ys)
+        | ys < xs = False
+        | ys > xs = True
+        | a > b = False
+        | otherwise = True
+    (Recurs _ _) <= (Simple _) = False
+    (Simple _) <= (Recurs _ _) = True
+    (Simple (ah:at)) <= (Simple (bh:bt))
+        | isUpper ah && not (isUpper bh) = False
+        | not (isUpper ah) && isUpper bh = True
+    (Simple a) <= (Simple b) = a <= b
+
+type HeaderLine = (String, IdentList)
+
 intoTuple :: IListItem -> (String, Maybe IdentList)
 intoTuple (Simple s) = (s, Nothing)
 intoTuple (Recurs s list) = (s, Just list)
+
+fromTuple :: (String, Maybe IdentList) -> IListItem
+fromTuple (s, mi) = maybe (Simple s) (Recurs s) mi
 
 importList :: Parser IdentList
 importList =
@@ -61,30 +86,38 @@ headerLine s =
 header = (,) <$> headerLine "module" <*> many (headerLine "import")
 
 cleanIdentList :: IdentList -> IdentList
-cleanIdentList = 
-    \case
-        All -> All
-        List list -> undefined 
-            where
-                m = M.fromListWith insertFn $ map intoTuple list
-                insertFn :: Maybe IdentList -> Maybe IdentList -> Maybe IdentList
-                insertFn _ (Just All) = Just All
-                insertFn (Just All) _ = Just All
-                insertFn Nothing o = o
-                insertFn o Nothing = o
-                insertFn (Just (List as)) (Just (List bs)) = Just . List . reverse . nub . reverse $ (as ++ bs)
+cleanIdentList All = All
+cleanIdentList (List ls) = List . reverse . sort . unique . map fromTuple . M.toList . makeMap $ ls
+    where
+        makeMap = M.fromListWith combineFn . map intoTuple
+        combineFn :: Maybe IdentList -> Maybe IdentList -> Maybe IdentList
+        combineFn _ (Just All) = Just All
+        combineFn (Just All) _ = Just All
+        combineFn Nothing o = o
+        combineFn o Nothing = o
+        combineFn (Just (List as)) (Just (List bs)) = Just . cleanIdentList . List $ as ++ bs
 
-formatModule :: (String, IdentList) -> String
+unique = reverse . nub . reverse
+
+cleanLines :: [HeaderLine] -> [HeaderLine]
+cleanLines = sortOn fst . map (second cleanIdentList) . M.toList . M.fromListWith combineFn
+    where
+        combineFn All _ = All
+        combineFn _ All = All
+        combineFn (List a) (List b) = List (a ++ b)
+
+formatModule :: HeaderLine -> String
 formatModule (name, exports) = "module " ++ name ++ " exposing " ++ formatIdentList exports
 
-formatImport :: (String, IdentList) -> String
+formatImport :: HeaderLine -> String
 formatImport (name, imports) = "import " ++ name ++ " exposing " ++ formatIdentList imports
 
 formatIdentList :: IdentList -> String
 formatIdentList list = "( " ++ maybe ".." body (intoMaybe list) ++ " )"
     where body = intercalate " , " . map ((\(s, ma) -> s ++ maybe "" ((' ':) . formatIdentList) ma) . intoTuple)
 
-formatAll m is = intercalate "\n" $ formatModule m : "\n" : map formatImport is
+formatAll :: HeaderLine -> [HeaderLine] -> [Char]
+formatAll m is = intercalate "\n" $ formatModule m : map formatImport (cleanLines is)
 
 parseFile :: String -> IO ()
 parseFile path =
