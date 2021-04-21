0ab140dc9597c0c54e7976b1dc7b493bcfff52bf
L Caraco <lbc.dna@gmail.com>
2021-04-21 10:16:59 -0500

    clean up

--- ImportCleaner.hs
+++ ImportCleaner.hs
@@ -9,7 +9,7 @@ import Data.Bifunctor ( Bifunctor(second) )
 import Data.List (find, isSuffixOf, nub, intercalate, sort, sortOn, isPrefixOf, groupBy)
 import qualified Data.Map.Strict as M
 import Data.Char (isUpper)
-import Control.Arrow (Arrow((&&&)))
+import Control.Arrow ( Arrow((&&&)) )
 import System.Directory ( getDirectoryContents )
 
 whitespace = " \t\n"
@@ -34,9 +34,6 @@ intoMaybe :: IdentList -> Maybe [IListItem]
 intoMaybe All = Nothing
 intoMaybe (List ls) = Just ls
 
-fromMaybe :: Maybe [IListItem] -> IdentList
-fromMaybe = maybe All List
-
 data IListItem
     = Simple String
     | Recurs String IdentList
@@ -46,35 +43,30 @@ instance Ord IListItem where
     (Recurs a xs) <= (Recurs b ys)
         | ys < xs = True
         | ys > xs = False
-        | a < b = True
-        | otherwise = False
+        | otherwise = a <= b
     (Recurs _ _) <= (Simple _) = True
     (Simple _) <= (Recurs _ _) = False
-    (Simple (ah:at)) <= (Simple (bh:bt))
+    (Simple a@(ah:at)) <= (Simple b@(bh:bt))
         | isUpper ah && not (isUpper bh) = True
         | not (isUpper ah) && isUpper bh = False
-    (Simple a) <= (Simple b) = a <= b
+        | otherwise = a <= b
 
 type HeaderLine = (String, Maybe IdentList)
 
-intoTuple :: IListItem -> (String, Maybe IdentList)
+intoTuple :: IListItem -> HeaderLine
 intoTuple (Simple s) = (s, Nothing)
 intoTuple (Recurs s list) = (s, Just list)
 
-fromTuple :: (String, Maybe IdentList) -> IListItem
+fromTuple :: HeaderLine -> IListItem
 fromTuple (s, mi) = maybe (Simple s) (Recurs s) mi
 
 importList :: Parser IdentList
 importList =
  do lexeme (char '(')
-    b <- importBody
+    b <- lexeme (All <$ string "..") <|> fmap List (importIdentifier `sepBy` lexeme (char ','))
     lexeme (char ')')
     return b
   where
-    importBody :: Parser IdentList
-    importBody = 
-        lexeme (All <$ string "..") <|> fmap List (importIdentifier `sepBy` lexeme (char ','))
-    importIdentifier :: Parser IListItem
     importIdentifier =
      do base <- identifier
         maybe (Simple base) (Recurs base) <$> optionMaybe importList
@@ -138,7 +130,6 @@ formatAll m = ((formatModule m ++ "\n\n") ++) . ungroup . number . cleanLines
         number = map (groupNumber &&& formatImport) 
         ungroup = intercalate "\n\n" . map (intercalate "\n" . map snd) . groupBy (\a b -> fst a == fst b)
 
-cleanFile :: String -> IO ()
 cleanFile path =
  do text <- readFile path
     case parse elmFile path text of
@@ -147,10 +138,10 @@ cleanFile path =
             print err
         Right (h, body) ->
          do let formatted = uncurry formatAll h
-            let !_body = body
+            let !_body = body  -- force eval before write
             writeFile path (formatted ++ "\n\n" ++ _body)
 
-cleanDirectory :: FilePath  -> IO ()
+cleanDirectory :: FilePath -> IO ()
 cleanDirectory dirPath =
  do paths <- getDirectoryContents dirPath
     let files = filter (isSuffixOf ".elm") paths
